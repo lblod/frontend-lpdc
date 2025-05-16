39999949c8bfc31660b1d6d35710c9bc80dec4d2
benjay10 <ben.behaeghe@redpencil.io>
2025-05-16 05:23:25 +0200

    More wiring of impersonation and session control
    Again, mostly copied from Loket

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,8 +5,48 @@ import config from 'frontend-lpdc/config/environment';
 export default class ApplicationController extends Controller {
   @service session;
   @service currentSession;
+  @service impersonation;
   @service router;
 
   appTitle = 'Lokale Producten- en Dienstencatalogus';
   loketUrl = !config.loketUrl.startsWith('{{') ? config.loketUrl : null;
+
+  get userInfo() {
+    let user;
+    let group;
+    let classification;
+
+    if (this.impersonation.isImpersonating) {
+      user = this.impersonation.originalAccount.gebruiker;
+      group = this.impersonation.originalGroup;
+      classification = group.belongsTo('classificatie').value();
+    } else {
+      user = this.currentSession.user;
+      group = this.currentSession.group;
+      classification = this.currentSession.groupClassification;
+    }
+
+    if (!user) {
+      return '';
+    }
+
+    let userInfo = user.fullName;
+    let groupInfo = '';
+
+    if (classification?.label) {
+      groupInfo += classification.label;
+    }
+
+    if (group?.naam) {
+      groupInfo += ` ${group.naam}`;
+    }
+
+    groupInfo.trim();
+
+    if (groupInfo.length) {
+      userInfo += ` - ${groupInfo}`;
+    }
+
+    return userInfo;
+  }
 }

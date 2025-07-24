829c5697607bad179e3869811baecc2c048aba1b
wolfez <wolfderechter@gmail.com>
2025-07-24 10:48:53 +0200

    add new existing instances overview page

---
+++ app/controllers/public-services/existing-instances.js
@@ -0,0 +1,35 @@
+import Controller from '@ember/controller';
+import { dropTask } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import ConfirmDeletionModal from 'frontend-lpdc/components/confirm-deletion-modal';
+import { tracked } from '@glimmer/tracking';
+
+export default class PublicServicesExistingInstancesController extends Controller {
+  @service router;
+  @service modals;
+  @service('public-service') publicServiceService;
+  @service('concept') conceptService;
+  @tracked sort = '-date-modified';
+  @tracked page = 0;
+
+  @dropTask
+  *createPublicService(conceptUuid) {
+    const concept = yield this.conceptService.loadConceptDetails(conceptUuid);
+    const publicServiceUuid =
+      yield this.publicServiceService.createPublicService(concept.uri);
+
+    this.router.transitionTo('public-services.details', publicServiceUuid);
+  }
+
+  @action
+  removePublicService(uri) {
+    console.log('Removing public service with URI:', uri);
+    this.modals.open(ConfirmDeletionModal, {
+      deleteHandler: async () => {
+        await this.publicServiceService.deletePublicService(uri);
+        this.router.refresh();
+      },
+    });
+  }
+}
